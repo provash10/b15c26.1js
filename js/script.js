1b28d7b66db454d3565876876834682be1bbcf1c
provash10 <provash20cb@gmail.com>
2024-10-16 18:41:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Problem 02:
+// Total donation money for flood effected people is 50lacs tk. 
+// Some donation money added from an International organization. 
+// The donation money from the organization is 20lacs.
+// Now I want to divide the money between 90 families. 
+// How much money will get each family?
+
+let total_donation = 5000000;
+let add_donation   = 2000000;
+
+let new_donation = total_donation + add_donation;
+
+let family = 90;
+
+let get_each_family = new_donation / family;
+
+console.log(get_each_family);
+
+// console.log("Each family will get: " + moneyPerFamily + " tk.");
+
